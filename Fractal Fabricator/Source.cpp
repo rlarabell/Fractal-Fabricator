9d392418e2df993fd163f87faaa4b9eb587fdf53
rlarabell <rlarabell@gmail.com>
2023-03-26 18:50:18 -0400

    - Some performance improvements - getting the number of threads dynamically with the hardware_concurrency() function, reserving size of some vectors, using async
    - Some changes for readability, i.e. using the pow() function and changing some ints to size_t because I guess that's a best practice for things that can't be negative.
    
    - This is a little faster than before I think (and runs better/less specific to my machine as it doesn't just assume 4 threads) but it's still quite slow. I'm looking into using GPU acceleration with CUDA (which would be limited to NVidia cards) or other GPU acceleration toolkits like OpenCL to speed this up more but I don't know anything about those yet really. If/when I get there I can modify this code some more and make an Angular frontend for it or something.

--- Fractal Fabricator/Source.cpp
+++ Fractal Fabricator/Source.cpp
@@ -1,217 +1,232 @@
 ﻿#include <iostream>
-#include <thread> 
-#include <complex> 
-#include <SFML/Graphics.hpp> 
+#include <vector>
+#include <complex>
+#include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <future>
 
 using namespace std;
 
+// Create a new pixel map for the image based on the specified width and height
 vector<sf::Uint8> createPixMap(int width, int height);
 
+// Create an array of complex numbers based on the specified width and height
 vector<complex<double>> createComplexNums(int width, int height);
 
+// Create a component of the complex numbers array
 void createComponent(vector<double>& component, int size);
 
-void buildPixelsArray(vector<int> intensities, vector<sf::Uint8>& pixels);
+// Convert the intensities array to a pixels array
+void buildPixelsArray(const vector<int>& intensities, vector<sf::Uint8>& pixels);
 
-void buildIntensitiesArray(int arrayStart, int arrayEnd, vector<complex<double>> complexNums, vector<int>& intensities);
+// Build an array of intensities for a subset of the complex numbers array
+void buildIntensitiesArray(size_t arrayStart, size_t arrayEnd, const vector<complex<double>>& complexNums, vector<int>& intensities);
 
-class Func
+class Func // Class representing the fractal function and its properties (TODO: Make dynamic)
 {
-	public:
-		double realModifier, imaginaryModifier; 
-		int threshold; 
+    public:
+        double realModifier, imaginaryModifier;
+        int threshold;
 
-		Func()
-		{
-			realModifier = -0.835;
-			imaginaryModifier = 0.2321;
-			threshold = 2;
-		}
+    Func() : realModifier(-0.654), imaginaryModifier(0.475), threshold(2) {}
 
-		int FuncCount(int count, complex<double> coords)
-		{
-			if (abs(coords) < threshold) 
-			{
-				if (count < 255) 
-					return FuncCount(count + 1, applyFunc(coords)); 
+    // Function to count the number of iterations of the function for a specific coordinate - this is recursive and called until
+    // the threshold value is reached or the function has been called 255 times
+    int FuncCount(int count, complex<double> coords) 
+    {
+        if (abs(coords) < threshold) 
+        {
+            return count < 255 ? FuncCount(count + 1, applyFunc(coords)) : count;
+        }
 
-				else 
-					return count;
-			}
+        else 
+        {
+            return count;
+        }
+    }
 
-			else 
-				return count; 
-		}
-
-	private:
-		//Function is f(x) = x^2 + realModifier + imaginaryModifier
-		complex<double> applyFunc(complex<double> coords)
-		{
-			double realNumber, imaginaryNumber;
-
-			//new real number = real^2 - imaginary^2 + realModifier (subtracting imaginary squared since i^2 is equal to -1)
-			realNumber = (real(coords) * real(coords)) - (imag(coords) * imag(coords)) + realModifier;
-
-			//new imaginary number = 2 * (real * imaginary) + imaginaryModifier cause FOIL and stuff...
-			imaginaryNumber = 2 * (imag(coords) * real(coords)) + imaginaryModifier;
-
-			coords = complex<double>(realNumber, imaginaryNumber); 
-
-			return coords; 
-		}
+    private:
+        // Function to apply the fractal function to a specific coordinate
+        complex<double> applyFunc(complex<double> coords) 
+        {
+            double realNumber = pow(real(coords), 2) - pow(imag(coords), 2) + realModifier;
+            double imaginaryNumber = 2 * (imag(coords) * real(coords)) + imaginaryModifier;
+            return complex<double>(realNumber, imaginaryNumber);
+        }
 };
 
-int main()
+int main() 
 {
-	int width, height;
+    int width, height;
 
-	sf::Image image;
-	sf::Texture imageTexture;
-	sf::Sprite imageSprite; 
+    sf::Image image;
+    sf::Texture imageTexture;
+    sf::Sprite imageSprite;
 
-	vector<sf::Uint8> pixels; 
+    vector <sf::Uint8> pixels;
 
-	sf::RenderWindow window; 
-	sf::View view; 
+    sf::RenderWindow window;
+    sf::View view;
 
-	window.create(sf::VideoMode::getDesktopMode(), "Fractal Fabricator", sf::Style::Fullscreen);
+    window.create(sf::VideoMode::getDesktopMode(), "Fractal Fabricator", sf::Style::Fullscreen);
 
-	view = window.getDefaultView();
-	window.setView(view);
+    view = window.getDefaultView();
+    window.setView(view);
 
-	width = window.getSize().x;
-	height = window.getSize().y;
+    width = window.getSize().x;
+    height = window.getSize().y;
 
-	imageTexture.create(width, height);
-	imageTexture.setSmooth(true);
+    imageTexture.create(width, height);
+    imageTexture.setSmooth(true);
 
-	pixels = createPixMap(width, height);
+    pixels = createPixMap(width, height);
 
     image.create(width, height, pixels.data());
 
-	imageTexture.update(image);
+    imageTexture.update(image);
 
-	imageSprite = sf::Sprite(imageTexture); 
+    imageSprite = sf::Sprite(imageTexture);
 
-	imageSprite.setOrigin(imageSprite.getLocalBounds().width / 2, imageSprite.getLocalBounds().height / 2);
+    imageSprite.setOrigin(imageSprite.getLocalBounds().width / 2, imageSprite.getLocalBounds().height / 2);
 
-	imageSprite.setPosition(((float)width / 2), ((float)height / 2));
+    imageSprite.setPosition(((float)width / 2), ((float)height / 2));
 
-	while (window.isOpen())
-	{
-		sf::Event currentEvent;
-		window.pollEvent(currentEvent);
+    while (window.isOpen()) 
+    {
+        sf::Event currentEvent;
+        window.pollEvent(currentEvent);
 
-		window.clear();
+        window.clear();
 
-		window.draw(imageSprite);
+        window.draw(imageSprite);
 
-		window.display();
+        window.display();
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
-	}
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) 
+        {
+            window.close();
+        }
+    }
 }
 
-vector<sf::Uint8> createPixMap(int width, int height)
+vector<sf::Uint8> createPixMap(int width, int height) 
 {
-	int size = width * height * 4;
+    size_t size = width * height * 4;
 
-	vector<sf::Uint8> pixels;
+    vector<sf::Uint8> pixels(size);
 
-	Func function;
+    Func function;
 
-	vector<complex<double>> complexNums;
+    vector<complex<double>> complexNums;
 
-	vector<int> intensitiesFirstFourth, intensitiesSecondFourth, intensitiesThirdFourth, intensitiesFourthFourth;
+    complexNums = createComplexNums(width, height);
 
-	complexNums = createComplexNums(width, height);
+    size_t num_threads = std::thread::hardware_concurrency();
 
-	int complexNumbersQuartered = complexNums.size()/4;
+    size_t chunk_size = complexNums.size() / num_threads;
 
-	thread threadFirstFourth, threadSecondFourth, threadThirdFourth, threadFourthFourth;
+    vector<vector<int>> threadIntensities(num_threads);
+    vector<std::future<void>> futures;
 
-	threadFirstFourth = thread(buildIntensitiesArray, 0, complexNumbersQuartered, complexNums, ref(intensitiesFirstFourth));
-	threadSecondFourth = thread(buildIntensitiesArray, complexNumbersQuartered+1, complexNumbersQuartered*2, complexNums, ref(intensitiesSecondFourth));
-	threadThirdFourth = thread(buildIntensitiesArray, complexNumbersQuartered*2+1, complexNumbersQuartered*3, complexNums, ref(intensitiesThirdFourth));
-	threadFourthFourth = thread(buildIntensitiesArray, complexNumbersQuartered*3+1, complexNums.size()-1, complexNums, ref(intensitiesFourthFourth));
-	
-	threadFirstFourth.join();
-	threadSecondFourth.join();
-	threadThirdFourth.join();
-	threadFourthFourth.join();
+    for (size_t i = 0; i < num_threads; ++i) 
+    {
+        size_t start = i * chunk_size;
+        size_t end = (i == num_threads - 1) ? complexNums.size() : (i + 1) * chunk_size;
 
-	buildPixelsArray(intensitiesFirstFourth, pixels);
-	buildPixelsArray(intensitiesSecondFourth, pixels);
-	buildPixelsArray(intensitiesThirdFourth, pixels);
-	buildPixelsArray(intensitiesFourthFourth, pixels);
+        // Preallocate the required space for each thread's intensities vector
+        threadIntensities[i].resize(end - start);
 
-	return pixels; 
+        // Use a lambda function to call buildIntensitiesArray with correct arguments
+        futures.push_back(std::async(std::launch::async, [start, end, &complexNums, &threadIntensities, i]()
+        {
+            buildIntensitiesArray(start, end, std::ref(complexNums), std::ref(threadIntensities[i]));
+        }));
+    }
+
+    for (auto& future : futures)
+    {
+        future.wait();
+    }
+
+    vector<int> mergedIntensities;
+
+    mergedIntensities.reserve(complexNums.size());
+
+    for (const auto& intensities : threadIntensities)
+    {
+        mergedIntensities.insert(mergedIntensities.end(), intensities.begin(), intensities.end());
+    }
+
+    buildPixelsArray(mergedIntensities, pixels);
+
+    return pixels;
 }
 
-void buildPixelsArray(vector<int> intensities, vector<sf::Uint8>& pixels)
+void buildPixelsArray(const vector<int>& intensities, vector<sf::Uint8>& pixels) 
 {
-	for (const auto& i : intensities)
-	{
-		pixels.push_back(255);   // r 
-		pixels.push_back(255);  //  g 
-		pixels.push_back(255); //   b 
-		pixels.push_back(i);  //    a
-	}
+    for (size_t i = 0; i < intensities.size(); ++i) 
+    {
+        size_t baseIndex = i * 4;
+        pixels[baseIndex] = 255;     // r
+        pixels[baseIndex + 1] = 255; // g
+        pixels[baseIndex + 2] = 255; // b
+        pixels[baseIndex + 3] = intensities[i]; // a
+    }
 }
 
-void buildIntensitiesArray(int arrayStart, int arrayEnd, vector<complex<double>> complexNums, vector<int>& intensities)
+void buildIntensitiesArray(size_t arrayStart, size_t arrayEnd, const vector<complex<double>>& complexNums, vector<int>& intensities) 
 {
-	Func function;
+    Func function;
 
-	for (int i = arrayStart; i < arrayEnd; i++)
-	{
-		intensities.push_back(function.FuncCount(0, complexNums[i]));
-	}
+    if (arrayEnd > complexNums.size() || arrayStart >= arrayEnd || intensities.size() != arrayEnd - arrayStart) 
+    {
+        return;
+    }
+
+    for (size_t i = arrayStart; i < arrayEnd; ++i) 
+    {
+        intensities[i - arrayStart] = function.FuncCount(0, complexNums[i]);
+    }
 }
 
-vector<complex<double>> createComplexNums(int width, int height)
+vector<complex<double>> createComplexNums(int width, int height) 
 {
-	complex<double> complexNumber; 
+    vector<complex<double>> complexArray(width * height);
 
-	vector<complex<double>> complexArray; 
+    vector<double> real, imaginary;
 
-	vector<double> real, imaginary;
+    createComponent(real, width);
+    createComponent(imaginary, height);
 
-	thread threadReal, threadImaginary;
+    for (int i = 0; i < height; i++) 
+    {
+        for (int j = 0; j < width; j++) 
+        {
+            complexArray[i * width + j] = complex<double>(real[j], imaginary[i]);
+        }
+    }
 
-	threadReal = thread(createComponent, ref(real), width);
-	threadImaginary = thread(createComponent, ref(imaginary), height);
-
-	threadReal.join();
-	threadImaginary.join();
-
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-			complexNumber = complex<double>(real[j], imaginary[i]);
-
-			complexArray.push_back(complexNumber);
-		}
-	}
-
-	return complexArray;
+    return complexArray;
 }
 
-void createComponent(vector<double>& component, int size)
+void createComponent(vector<double>& component, int size) 
 {
-	for (int i = -1; i < size - 1; i++) 
-	{
-		if (component.empty() == true) // if no values have been computed yet
-			component.push_back(i); // first real value set to -1
-
-		else 
-			component.push_back(component.back() + 2.0 / (size - 1)); // otherwise the next value of the set
-																	  // is computed as 2.0/(size-1) more than the
-																	 // previous value
-	}
+    for (int i = -1; i < size - 1; i++) 
+    {
+        if (component.empty()) // if no values have been computed yet
+        { 
+            component.push_back(i); // first real value set to -1
+        }
+        else 
+        {
+            component.push_back(component.back() + 2.0 / (size - 1)); // otherwise the next value of the set
+                                                                      // is computed as 2.0/(size-1) more than the
+                                                                      // previous value
+        }
+    }
 }
